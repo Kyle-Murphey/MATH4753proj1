2f6d2eafcd46a1f6b28f2e56fefa0d76b75bb22c
Kyle-Murphey <kyle_murphey@live.com>
2020-02-07 18:31:01 -0600

    Initial files

---
+++ R/myf.R
@@ -0,0 +1,14 @@
+#' Square Function
+#'
+#' takes a vector and squares components
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples x = 1:30; myf(x);
+myf = function(x)
+{
+  x^2
+}
